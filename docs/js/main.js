864c9d8ea1a2553319a5cf50387618794d195e4a
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-08-06 11:39:45 +0200

    Desarrollo del tercer gráfico

--- docs/js/main.js
+++ docs/js/main.js
@@ -285,21 +285,31 @@ function getThirdChart() {
     let localFile = './data/chart-three.csv';
     d3.csv(localFile, function(d) {
         return {
-            tipo: d.tipo,
-            tipo_eje: d.tipo_eje,
-            porcentaje: +d['porcentaje'].replace(/,/g, '.') * 100
+            tipo: d.city,
+            'within_100m': +d['within_100m'].replace(/,/g, '.').replace('%',''),
+            'within_100_200m': +d['within_100_200m'].replace(/,/g, '.').replace('%',''),
+            'within_200_400m': +d['within_200_400m'].replace(/,/g, '.').replace('%',''),
+            'within_400m_1km': +d['within_400m_1km'].replace(/,/g, '.').replace('%','')
         }
     }, function(error, data) {
         if (error) throw error;
-
+        
+        data = data.reverse();
         //Creación del elemento SVG en el contenedor
-        let margin = {top: 5, right: 5, bottom: 25, left: 110};
+        let margin = {top: 5, right: 17.5, bottom: 25, left: 90};
         let {width, height, chart} = setChart(chartBlock, margin);
+
+        let keys = data.columns.slice(1);
+        
+        //Colores
+        let z = d3.scaleOrdinal()
+            .range([more_colors_first, more_colors_second, more_colors_third, more_colors_fourth])
+            .domain(keys);
 
         //Disposición del eje X
         let x = d3.scaleLinear()
-            .domain(d3.extent(data, function(d) { return d.porcentaje; }))
-            .range([0, width])
+            .domain([0,100])
+            .range([10, width])
             .nice();
 
         //Estilos para eje X
@@ -325,27 +335,13 @@ function getThirdChart() {
 
         //Disposición del eje Y
         let y = d3.scaleBand()
-            .domain(data.map(function(d) { return d.tipo_eje + '-' + d.tipo; }))
+            .domain(data.map(function(d) { return d.tipo; }))
             .range([height, 0]);
 
         let yAxis = function(svg){
-            svg.call(d3.axisLeft(y).tickFormat(function(d) { return d.split('-')[0]; }))
+            svg.call(d3.axisLeft(y).tickFormat(function(d) { return d; }))
             svg.call(function(g){g.selectAll('.tick line').remove()})
-            svg.call(function(g){g.select('.domain').remove()})
-            svg.call(function(g){g.selectAll('.tick text').style('cursor','default')})
-            svg.call(function(g){g.selectAll('.tick text').on('mouseenter mousedown mousemove mouseover', function(d) {
-                //Texto tooltip
-                let html = `<p class="chart__tooltip--title">${d.split('-')[1]}</p>`;                
-                tooltip.html(html);
-
-                //Tooltip
-                positionTooltip(window.event, tooltip);
-                getInTooltip(tooltip);
-            })})
-            svg.call(function(g){g.selectAll('.tick text').on('mouseleave', function(d) { 
-                //Quitamos el tooltip
-                getOutTooltip(tooltip); 
-            })});
+            svg.call(function(g){g.select('.domain').remove()});
         }        
         
         chart.append("g")
@@ -364,33 +360,34 @@ function getThirdChart() {
         });
 
         function initChart() {
-            chart.selectAll(".bar")
-                .data(data)
+            chart.selectAll('.bar')
+                .data(d3.stack().keys(keys)(data))
                 .enter()
-                .append("rect")
-                .attr('class', function(d, i) { return `bar bar-${i}`; })
-                .style('fill', one_color)
-                .attr("x", function (d) {
-                    return x(0);
-                })
-                .attr("y", function (d) {
-                    return y(d.tipo_eje + '-' + d.tipo) + y.bandwidth() / 4;
-                })            
+                .append("g")
+                .attr("fill", function(d) { return z(d.key); })
+                .attr("class", function(d) { return 'g_rect rect-' + d.key; })
+                .selectAll("rect")
+                .data(function(d) { return d; })
+                .enter()
+                .append("rect")                
+                .attr("y", function(d) { return y(d.data.tipo) + y.bandwidth() / 4; })
+                .attr("x", function(d) { return x(0); })
                 .attr("height", y.bandwidth() / 2)
                 .on('mouseenter mousedown mousemove mouseover', function(d, i, e) {
-                    let css = e[i].getAttribute('class').split('-')[1];
+                    let css = e[i].parentNode.getAttribute('class').split('-')[1];
+                    let currentData = {city: d.data.tipo, data: d.data[`${css}`]};
                     //Texto
-                    let html = `<p class="chart__tooltip--title">${d.tipo}</p>
-                                <p class="chart__tooltip--text">${numberWithCommas(d.porcentaje.toFixed(2))}%</p>`; //Solucionar recogida de información
+                    let html = `<p class="chart__tooltip--title">${currentData.city}</p>
+                                <p class="chart__tooltip--text">${currentData.data}%</p>`; //Solucionar recogida de información
 
                     tooltip.html(html);
 
                     //Posibilidad visualización línea diferente
-                    let bars = chartBlock.selectAll('.bar');
+                    let bars = chartBlock.selectAll('.g_rect');
                     
                     bars.each(function() {
-                        this.style.opacity = '0.4';
-                        if(this.getAttribute('class').indexOf(`bar-${css}`) != -1) {
+                        this.style.opacity = '0.2';
+                        if(this.getAttribute('class').indexOf(`rect-${css}`) != -1) {
                             this.style.opacity = '1';
                         }
                     });
@@ -401,7 +398,7 @@ function getThirdChart() {
                 })
                 .on('mouseout', function(d, i, e) {
                     //Quitamos los estilos de la línea
-                    let bars = chartBlock.selectAll('.bar');
+                    let bars = chartBlock.selectAll('.g_rect');
                     bars.each(function() {
                         this.style.opacity = '1';
                     });
@@ -411,19 +408,71 @@ function getThirdChart() {
                 })
                 .transition()
                 .duration(3000)
-                .attr("x", function (d) {
-                    return x(Math.min(0, d.porcentaje));
-                })
-                .attr("width", function (d) {
-                    return Math.abs(x(d.porcentaje) - x(0));
-                });
-        }                   
+                .attr("x", function(d) { return x(d[0]); })	
+                .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+        }
+
+        // function initChart() {
+        //     chart.selectAll(".bar")
+        //         .data(data)
+        //         .enter()
+        //         .append("rect")
+        //         .attr('class', function(d, i) { return `bar bar-${i}`; })
+        //         .style('fill', one_color)
+        //         .attr("x", function (d) {
+        //             return x(0);
+        //         })
+        //         .attr("y", function (d) {
+        //             return y(d.tipo_eje + '-' + d.tipo) + y.bandwidth() / 4;
+        //         })            
+        //         .attr("height", y.bandwidth() / 2)
+                // .on('mouseenter mousedown mousemove mouseover', function(d, i, e) {
+                //     let css = e[i].getAttribute('class').split('-')[1];
+                //     //Texto
+                //     let html = `<p class="chart__tooltip--title">${d.tipo}</p>
+                //                 <p class="chart__tooltip--text">${numberWithCommas(d.porcentaje.toFixed(2))}%</p>`; //Solucionar recogida de información
+
+                //     tooltip.html(html);
+
+                //     //Posibilidad visualización línea diferente
+                //     let bars = chartBlock.selectAll('.bar');
+                    
+                //     bars.each(function() {
+                //         this.style.opacity = '0.4';
+                //         if(this.getAttribute('class').indexOf(`bar-${css}`) != -1) {
+                //             this.style.opacity = '1';
+                //         }
+                //     });
+
+                //     //Tooltip
+                //     positionTooltip(window.event, tooltip);
+                //     getInTooltip(tooltip);
+                // })
+                // .on('mouseout', function(d, i, e) {
+                //     //Quitamos los estilos de la línea
+                //     let bars = chartBlock.selectAll('.bar');
+                //     bars.each(function() {
+                //         this.style.opacity = '1';
+                //     });
+
+                //     //Quitamos el tooltip
+                //     getOutTooltip(tooltip); 
+                // })
+        //         .transition()
+        //         .duration(3000)
+        //         .attr("x", function (d) {
+        //             return x(Math.min(0, d.porcentaje));
+        //         })
+        //         .attr("width", function (d) {
+        //             return Math.abs(x(d.porcentaje) - x(0));
+        //         });
+        // }                   
     });
 }
 
 getFirstChart();
 //getSecondChart();
-//getThirdChart();
+getThirdChart();
 
 /* Visualization helpers */
 function wrap(text, width) {
